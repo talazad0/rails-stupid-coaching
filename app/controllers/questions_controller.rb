3e8c0a7cd0adc443c8bbb84d2a9295720419a3d5
Timothy AlAzawie <t.alazawie@yahoo.co.uk>
2023-05-17 15:32:20 +0100

    Need to finish question view and style

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    if @question.blank?
+      @answer = 'huh?'
+    elsif @question.last == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @answer = 'Well done'
+    else
+      @answer = 'I dont care go to work'
+    end
+  end
+end
